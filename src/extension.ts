15aaf525fd932d0ecb3fb15503bfee59f6142e85
Saarang Agarwal <agarwalsaarang@yahoo.com>
2025-03-25 20:37:46 -0400

    create sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "devhelperai" is now active!');
+
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+		"devhelperai-sidebar",
+		sidebarProvider
+		)
+	);
 
 	context.subscriptions.push(vscode.commands.registerCommand('devhelperai.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from DevHelperAI!');
--- src/getNonce.ts
+++ src/getNonce.ts
@@ -1,4 +1,4 @@
-function getNonce() {
+export function getNonce() {
 	let text = '';
 	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	for (let i = 0; i < 32; i++) {
